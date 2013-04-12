affe648fcbaeaff8e8fd5111a7905472335f4345
Samuel Kadolph <samuel@kadolph.com>
2013-04-12 15:17:59 -0400

    Whoops, shouldn't use options

--- lib/rainbows/rails.rb
+++ lib/rainbows/rails.rb
@@ -11,13 +11,13 @@ module Rack
     class Rainbows
       class << self
         def run(app, options = {})
-          options = {}
-          options[:listeners] = ["#{options[:Host]}:#{options[:Port]}"]
-          options[:worker_processes] = (ENV["RAINBOWS_WORKERS"] || "1").to_i
-          options[:timeout] = 31 * 24 * 60 * 60
+          rainbows_options = {}
+          rainbows_options[:listeners] = ["#{options[:Host]}:#{options[:Port]}"]
+          rainbows_options[:worker_processes] = (ENV["RAINBOWS_WORKERS"] || "1").to_i
+          rainbows_options[:timeout] = 31 * 24 * 60 * 60
 
-          ::Rainbows::Launcher.daemonize!(options) if options[:daemonize]
-          ::Rainbows::HttpServer.new(app, options).start.join
+          ::Rainbows::Launcher.daemonize!(rainbows_options) if options[:daemonize]
+          ::Rainbows::HttpServer.new(app, rainbows_options).start.join
         end
       end
     end
--- lib/rainbows/rails/version.rb
+++ lib/rainbows/rails/version.rb
@@ -1,5 +1,5 @@
 module Rainbows
   module Rails
-    VERSION = "1.0.0"
+    VERSION = "1.0.1"
   end
 end
